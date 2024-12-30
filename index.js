a73969faa0bb5ca9a15dc273f1f4446d136d8742
BhargavMaru07 <bhargavhamir@gmail.com>
2024-12-30 21:56:29 +0530

    map setup

--- index.js
+++ index.js
@@ -11,12 +11,19 @@ const io = socket(server);
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("Home.ejs");
 });
 
+io.on("connection",function(socket){
+  console.log("connected");
+
+  socket.on("send-location",function(data){
+    io.emit("receive-location",{id:socket.id,...data})
+  });
+})
+
 server.listen(PORT, () => console.log("Server is listening on Port : ", PORT));
 
-//https://real-time-tracker-gamma-sage.vercel.app/
